4aa4bc99043f6e2fb170e5b2355d32040db76001
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-08-02 16:51:05 -0500

    added Owner model, view, and controllers to render list of owners and details page of each owner along with updating nav bar

---
+++ CatStroll/Models/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatStroll.Models
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please give your cat a name to be called :)")]
+        [MaxLength(35)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Breed { get; set; }
+
+        [Required(ErrorMessage = "Please give notes about your cat! :D")]
+        public string Notes { get; set; }
+
+        [Required(ErrorMessage = "Provide a selfie of your cute cat for us to identify!")]
+        [DisplayName("Image URL")]
+        public string ImageUrl { get; set; }
+        public int OwnerId { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
