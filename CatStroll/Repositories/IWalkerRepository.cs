28741b88b2493cf734518c801500dbb0fdb6ddf8
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-08-02 14:50:27 -0500

    initial commit

---
+++ CatStroll/Repositories/IWalkerRepository.cs
@@ -0,0 +1,12 @@
+﻿using CatStroll.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace CatStroll.Repositories
+{
+    public interface IWalkerRepository
+    {
+        List<Walker> GetAllWalkers();
+        Walker GetWalkerById(int id);
+    }
+}
